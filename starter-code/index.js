14a9faf48f258fdb96debe088966fda7f400c9a5
ingridpitta <ingridpitta056@gmail.com>
2020-03-07 14:05:28 -0300

    LAB | Intro Node [Done]

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    (this.items = [].sort()), (this.length = 0);
+  }
 
-  add(item) {}
+  add(item) {
+    const arr = this.items;
+    arr.push(item);
 
-  get(pos) {}
+    this.items = arr.sort((a, b) => {
+      return a > b;
+    });
+    this.length = arr.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos <= this.length - 1) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => a + b);
+    }
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return parseInt(
+        Math.ceil(this.items.reduce((a, b) => (b + a) / this.length))
+      );
+    }
+  }
 }
 
 module.exports = SortedList;
